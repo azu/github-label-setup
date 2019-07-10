03c6cccd56855b3f5642d85da1916cf2438491fc
azu <azuciao@gmail.com>
2019-07-11 00:03:56 +0900

    fix readme

--- scripts/readme-labels.js
+++ scripts/readme-labels.js
@@ -1,6 +1,6 @@
 const labels = require("../labels");
 const toMarkdown = (label) => {
-    return `- ![#${label.color}](https://placehold.it/15/${label.color}/000000?text=+) ${label.name} - ${label.description}`;
+    return `- ![#${label.color}](https://placehold.it/15/${label.color}/000000?text=+) ${label.name} ${label.description ? "-" + label.description : ""}`;
 };
 
 const markdown = labels.map(toMarkdown).join("\n");
