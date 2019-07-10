ba726b2b4ff98e9cf60b3e876d5483a5d50ac6ef
azu <azu@users.noreply.github.com>
2019-07-10 23:56:51 +0900

    feat(label): Update labels (#4)
    - add description
    - add "Type: CI" label
    - add "Need More Info" label
    - update github-labe-sync-@1.4.1

---
+++ scripts/readme-labels.js
@@ -0,0 +1,7 @@
+const labels = require("../labels");
+const toMarkdown = (label) => {
+    return `- ![#${label.color}](https://placehold.it/15/${label.color}/000000?text=+) ${label.name} - ${label.description}`;
+};
+
+const markdown = labels.map(toMarkdown).join("\n");
+console.log(markdown);
