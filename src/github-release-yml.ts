e0e099b30d2a8d68e292f5feda74c097e4495266
azu <azuciao@gmail.com>
2023-01-08 13:30:14 +0900

    Revert "feat: add --addReleaseYml flag (#14)"
    This reverts commit 9d711cd80392eb131789f6b5eb5c0dc8cb6676d4.

--- src/github-release-yml.ts
+++
@@ -1,8 +0,0 @@
-import * as fs from "fs/promises";
-import path from "path";
-
-export const writeGitHubReleaseYaml = async (outputFilePath: string) => {
-    const relaseYaml = await fs.readFile(path.join(__dirname, "../.github/release.yml"));
-    await fs.mkdir(path.dirname(outputFilePath), { recursive: true });
-    await fs.writeFile(outputFilePath, relaseYaml, "utf-8");
-}
