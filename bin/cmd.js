7d7c303c903a79154b40e2ff87972b151266616c
azu <azuciao@gmail.com>
2023-01-08 13:29:47 +0900

    esm

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
 'use strict';
-const meow = require("meow");
-const { setupLabels } = require("../lib/github-label-setup");
+import meow from "meow";
+import path from "path";
+import { setupLabels } from "../lib/github-label-setup.js";
+import { writeGitHubReleaseYaml } from "../lib/github-release-yml.js";
 
+/*
+ {
+ input: ['unicorns'],
+ flags: {rainbow: true},
+ ...
+ }
+ */
 const cli = meow(`
     Usage
       $ github-label-setup --token xxx
@@ -11,16 +20,33 @@ const cli = meow(`
 
       -h, --help                  [Boolean] output usage information
       -l, --labels <path>         [Path:String] the path to look for the label configuration in. Default: labels.json
-      --token <token>             [String] a GitHub access token (also settable with a GITHUB_ACCESS_TOKEN environment variable)
+      --token <token>             [String] a GitHub access token (also settable with a GITHUB_TOKEN or GITHUB_ACCESS_TOKEN environment variable)
       -d, --dry-run               [Boolean] calculate the required label changes but do not apply them
       -A, --allow-added-labels    [Boolean] allow additional labels in the repo, and don't delete them
+      
+    GitHub Release Options
+      
+      --addReleaseYml             [Boolean] add a .github/release.yml file
+      --addReleaseYmlOutputPath   [Path:String] the path to write the .github/release.yml file to. Default: .github/release.yml
+
+    Examples
+        # Overwrite labels in the current repository
+        $ github-label-setup --token "$GITHUB_TOKEN"
+        # Add labels to the current repository
+        $ github-label-setup --token "$GITHUB_TOKEN" --allow-added-labels
+        # Add .github/release.yml to integrate with GitHub Releases
+        $ github-label-setup --addReleaseYml
 
 `, {
-
+    
+    importMeta: import.meta,
     flags: {
         dryRun: {
             type: 'boolean',
             alias: 'd'
+        },
+        token: {
+            type: "string",
         },
         labels: {
             type: 'string',
@@ -29,18 +55,30 @@ const cli = meow(`
         allowAddedLabels: {
             type: 'boolean',
             alias: 'A'
+        },
+        addReleaseYml: {
+            type: "boolean",
+            default: false
+        },
+        addReleaseYmlOutputPath: {
+            type: "string",
+            default: path.join(process.cwd(), ".github/release.yml")
         }
     }
 });
-/*
- {
- input: ['unicorns'],
- flags: {rainbow: true},
- ...
- }
- */
 
-setupLabels(cli.flags).catch(function(error){
-    console.error(error);
-    cli.showHelp();
-});
+if (cli.flags.addReleaseYml) {
+    const outputPath = cli.flags.addReleaseYmlOutputPath;
+    if (!outputPath) {
+        throw new Error("addReleaseYmlOutputPath must not be empty");
+    }
+    writeGitHubReleaseYaml(outputPath).catch(function (error) {
+        console.error(error);
+        cli.showHelp();
+    });
+} else {
+    setupLabels(cli.flags).catch(function (error) {
+        console.error(error);
+        cli.showHelp();
+    });
+}
--- src/github-label-setup.ts
+++ src/github-label-setup.ts
@@ -1,10 +1,17 @@
 import assert from "assert";
 import githubLabelSync, { LabelInfo } from "github-label-sync";
 import getRemoteOriginUrl from "git-remote-origin-url";
-import path from "path";
 // @ts-expect-error - no type
 import tryResolve from "try-resolve";
-import { fromUrl } from "hosted-git-info";
+import hostGitInto from "hosted-git-info";
+
+import url from "node:url";
+import path from "node:path";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const __filename__ = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename__);
+const { fromUrl } = hostGitInto;
 
 /**
  * @returns {Promise.<{ owner: string|null, repo: string|null}>}
@@ -28,38 +35,36 @@ const getRepositoryInfo = () => {
  * @param allowAddedLabels
  * @returns {Promise}
  */
-export function setupLabels({
-                               token,
-                               repo,
-                               labels = path.join(__dirname, "..", "labels.json"),
-                               dryRun,
-                               allowAddedLabels
-                           }: {
+export async function setupLabels({
+                                      token,
+                                      repo,
+                                      labels = path.join(__dirname, "..", "labels.json"),
+                                      dryRun,
+                                      allowAddedLabels
+                                  }: {
     token: string;
     repo: string;
     labels: string
     dryRun: boolean;
     allowAddedLabels: boolean
 }) {
-    const repoPromise = repo ? Promise.resolve(repo) : getRepositoryInfo();
-    return repoPromise.then(repoPath => {
-        assert(repoPath, "repo should be needed.");
-        const labelFilePath = tryResolve(labels) || tryResolve(path.resolve(process.cwd(), labels));
-        if (!labelFilePath) {
-            throw new Error(`Not found label file: ${labels}`);
-        }
-        const labelJSON: LabelInfo[] = require(labelFilePath);
-        const accessToken = token || process.env.GITHUB_ACCESS_TOKEN;
-        if (!accessToken) {
-            throw new Error("GitHub Access Token is not defined");
-        }
-        return githubLabelSync({
-            accessToken: accessToken,
-            repo: repoPath,
-            labels: labelJSON,
-            dryRun,
-            allowAddedLabels,
-            log: console,
-        });
+    const repoPath = await (repo ? Promise.resolve(repo) : getRepositoryInfo());
+    assert(repoPath, "repo should be needed.");
+    const labelFilePath = tryResolve(labels) || tryResolve(path.resolve(process.cwd(), labels));
+    if (!labelFilePath) {
+        throw new Error(`Not found label file: ${labels}`);
+    }
+    const labelJSON: LabelInfo[] = require(labelFilePath);
+    const accessToken = token ?? process.env.GITHUB_TOKEN ?? process.env.GITHUB_ACCESS_TOKEN;
+    if (!accessToken) {
+        throw new Error("GitHub Access Token is not defined");
+    }
+    return githubLabelSync({
+        accessToken: accessToken,
+        repo: repoPath,
+        labels: labelJSON,
+        dryRun,
+        allowAddedLabels,
+        log: console,
     });
 }
