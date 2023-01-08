85c267fda1197a0d6e28ff69637ba85c8a16f52e
azu <azuciao@gmail.com>
2023-01-08 14:18:17 +0900

    fix: script update

--- scripts/readme-labels.js
+++ scripts/readme-labels.js
@@ -1,6 +1,8 @@
-const labels = require("../labels");
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const labels = require("../labels.json");
 const toMarkdown = (label) => {
-    return `- ![#${label.color}](https://via.placeholder.com/15/${label.color}/000000?text=+) ${label.name} ${label.description ? "- " + label.description : ""}`;
+    return `- ![#${label.color}](https://placehold.co/15x15/${label.color}/${label.color}.png) ${label.name} ${label.description ? "- " + label.description : ""}`;
 };
 
 const markdown = labels.map(toMarkdown).join("\n");
